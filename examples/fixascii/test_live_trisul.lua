22f1f318d5dc1d3a077260e25b7498517645c77c
vivekrajan <vivek@unleashnetworks.com>
2018-05-31 14:34:58 +0530

    formatting

--- examples/fixascii/test_live_trisul.lua
+++ examples/fixascii/test_live_trisul.lua
@@ -5,8 +5,6 @@
 -- PURPOSE:     Hook into Trisul's TCP reassembly engine 
 -- DESCRIPTION: Shows how to hool FIX .lua into Trisul's reassembly engine 
 --
--- 
-
 local FixDissector=require'fixp'
 local PDURec=require'pdurecord'
 
@@ -18,7 +16,7 @@ TrisulPlugin = {
   },
 
   onload=function()
-	T.fixflows={} 
+    T.fixflows={} 
   end,
 
   -- reassembly_handler block
@@ -29,30 +27,28 @@ TrisulPlugin = {
     -- 
     onpayload = function(engine, timestamp, flowkey, direction, seekpos, buffer) 
 
-		-- Per Flow : protocol detection 
-		-- if start of flow is not FIXT marker, then ignore entire flow 
-		if seekpos==0 then
-			if   buffer:tostring(0,6)=="8=FIXT" then 
-				T.fixflows[flowkey:id() ]= {
-					[0]=PDURec.new("fixp", FixDissector.new()),
-					[1]=PDURec.new("fixp", FixDissector.new())}
-			else 
-				T.fixflows[flowkey:id() ]=nil
-				return
-			end
-		else
-			if T.fixflows[flowkey:id() ]==nil then  return end 
-		end 
+      -- Per Flow : protocol detection 
+      -- if start of flow is not FIXT marker, then ignore entire flow 
+      if seekpos==0 then
+        if   buffer:tostring(0,6)=="8=FIXT" then 
+          T.fixflows[flowkey:id() ]= {
+            [0]=PDURec.new("fixp", FixDissector.new()),
+            [1]=PDURec.new("fixp", FixDissector.new())}
+        else 
+          T.fixflows[flowkey:id() ]=nil
+          return
+        end
+      else
+        if T.fixflows[flowkey:id() ]==nil then  return end 
+      end 
 
-		local fixDissector = T.fixflows[flowkey:id()][direction]
-		fixDissector:push_chunk(seekpos,buffer:tostring())
-
+      local fixDissector = T.fixflows[flowkey:id()][direction]
+      fixDissector:push_chunk(seekpos,buffer:tostring())
     end,
 
-	onterminate=function(engine,timestamp,flowkey)
-		T.fixflows[flowkey]=nil 
-	end, 
-
+    onterminate=function(engine,timestamp,flowkey)
+      T.fixflows[flowkey]=nil 
+    end, 
   },
-
 }
+
